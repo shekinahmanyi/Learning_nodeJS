85368ed2a5155f15c8902cf0198ad70c45e5b78b
shekinahmanyi <shekinahmanyi@gmail.com>
2023-03-31 18:18:05 +0100

    concatenating paths

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -12,3 +12,6 @@ console.log(path,extname(__filename));
 
 //Create Path Object 
 console.log(path.parse(__filename).base);
+
+//Concatenate paths
+console.log(path.join(__dirname,'test','hello.html'));
